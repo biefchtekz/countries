147cda5ebd6933fef8694c8e81bad72211b3a562
biefchtekz <sydoruk2oo3@gmail.com>
2022-08-02 18:53:20 +0200

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
+import {MainLayoutComponent} from "./components/main-layout/main-layout.component";
+import {RegionComponent} from "./components/region/region.component";
+import {CountryComponent} from "./components/country/country.component";
+import {AllRegionsComponent} from "./components/all-regions/all-regions.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', component: AllRegionsComponent},
+      {path: 'region', component: RegionComponent},
+      {path: 'country', component: CountryComponent}
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    }
+  )],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { AllRegionsComponent } from './components/all-regions/all-regions.component';
+import { RegionComponent } from './components/region/region.component';
+import { CountryComponent } from './components/country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    AllRegionsComponent,
+    RegionComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule,
